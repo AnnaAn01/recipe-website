d87a4206ab6590cd6a139e9c8d44d5846617d4f7
Anna An <arzumanyan.an@gmail.com>
2022-06-21 17:00:05 +0400

    adding the footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./components/HomePage/HomePage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HomePage />
+      <Footer />
     </div>
   );
 }
