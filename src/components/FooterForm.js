3fe3f65df1d8c3f689fe1fb0444d17a151c71caa
Anna An <arzumanyan.an@gmail.com>
2022-06-24 07:54:59 +0400

    connecting the email form functionality

--- src/components/FooterForm.js
+++ src/components/FooterForm.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FooterForm.css";
 import { useForm, ValidationError } from "@formspree/react";
 
 const FooterForm = () => {
   const [state, handleSubmit] = useForm("mgedbypa");
+  const [click, setClick] = useState(false);
 
-  //   const onSubmit = (e) => {
-  //     e.preventDefault();
-  //     console.log(e);
-  //   };
+  const handleClick = () => {
+    setClick(true);
+  };
 
   return (
     <div className="footer__newsletter-wrapper">
@@ -43,12 +43,60 @@ const FooterForm = () => {
               placeholder="Email"
             />
           </div>
-          <button className="form__items form__button" type="submit">
+          <button
+            className="form__items form__button"
+            type="submit"
+            onClick={() => {
+              handleClick();
+            }}
+          >
             Submit
           </button>
         </form>
 
-        {!handleSubmit ? (
+        {!click ? (
+          <div className="hidden">Before</div>
+        ) : click && state.succeeded ? (
+          <div
+            className="newsletter__status-success"
+            id="newsletter__submit-status"
+          >
+            Your email has been submitted!
+          </div>
+        ) : click && state.submitting ? (
+          <div class="lds-ring">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        ) : (
+          <div
+            className="newsletter__status-fail"
+            id="newsletter__submit-status"
+          >
+            Please, enter a valid email.
+          </div>
+        )}
+
+        {/*
+        {state.succeeded ? (
+          <div
+            className="newsletter__status-success"
+            id="newsletter__submit-status"
+          >
+            Your email has been submitted!
+          </div>
+        ) : (
+          <div
+            className="newsletter__status-fail"
+            id="newsletter__submit-status"
+          >
+            An error occurred!
+          </div>
+        )}
+*/}
+        {/*{handleSubmit ? (
           <div className="hidden">Before</div>
         ) : state.succeeded ? (
           <div
@@ -64,7 +112,7 @@ const FooterForm = () => {
           >
             An error occurred!
           </div>
-        ) : null}
+        ) : null}*/}
       </div>
     </div>
   );
