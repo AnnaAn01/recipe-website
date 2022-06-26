9a7ab87611675868baa002948cdad46b9cdcd865
Anna An <arzumanyan.an@gmail.com>
2022-06-26 15:24:21 +0400

    styling about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./AboutPage.css";
+import { BsSearch } from "react-icons/bs";
+
+import homepageMain1 from "../../images-src/homepage-main-1.jpg";
 
 const AboutPage = () => {
-  return <div className="about">AboutPage</div>;
+  return (
+    <>
+      <section className="homepage-first__container">
+        <div className="homepage-first__wrapper">Something here</div>
+      </section>
+    </>
+  );
 };
 
 export default AboutPage;
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -7,116 +7,111 @@ import homepageMain1 from "../../images-src/homepage-main-1.jpg";
 const HomePage = () => {
   return (
     <>
-      <div className="homepage">
-        <div className="">
-          <div className="homepage__title-area">
-            <h1 className="homepage__title-h1">
-              SIMPLE AND QUICK RECIPES FOR EVERYDAY LIFE
-            </h1>
-          </div>
-          <section className="homepage-first__container">
-            <div className="homepage-first__wrapper">
-              <div className="homepage-first__card">
-                <div className="card__img card__img-1">
-                  <div className="card__img--overlay"></div>
-                  <p className="card-label">PASTA</p>
-                </div>
-              </div>
-              <div className="homepage-first__card">
-                <div className="card__img card__img-2">
-                  <div className="card__img--overlay"></div>
-                  <p className="card-label">SNACKS</p>
-                </div>
-              </div>
-              <div className="homepage-first__card">
-                <div className="card__img card__img-3">
-                  <div className="card__img--overlay"></div>
-                  <p className="card-label">OVEN</p>
-                </div>
-              </div>
-              <div className="homepage-first__card">
-                <div className="card__img card__img-4">
-                  <div className="card__img--overlay"></div>
-                  <p className="card-label">SPICY</p>
-                </div>
-              </div>
-            </div>
-            <div className="search-container">
-              <BsSearch className="search-icon" />
-              <input
-                className="search-input"
-                type="text"
-                placeholder="Search our recipes"
-              />
-              <span className="search__or-span">OR</span>
-              <span className="search__view-all-span">View all Recipes</span>
-            </div>
-          </section>
-
-          <section className="homepage-second__container">
-            <div className="homepage-second__wrapper">
-              <h1 className="homepage-second__section-title">
-                Latest Quick Recipes
-              </h1>
-              {/*Second section cards */}
-              <div className="homepage-second__card">
-                <div className="second-card__img second-card__img-1">
-                  <div className="card__img--overlay"></div>
-                </div>
-                <div className="second-card__text ">
-                  <div className="second-card__title">Food name here</div>
-                  <div className="second-card__description">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s.
-                  </div>
-                  <a href="#" className="second-card__button-continue">
-                    Continue Reading
-                  </a>
-                </div>
-              </div>
-              <div className="homepage-second__devider-line"></div>
-              <div className="homepage-second__card">
-                <div className="second-card__img second-card__img-2">
-                  <div className="card__img--overlay"></div>
-                </div>
-                <div className="second-card__text ">
-                  <div className="second-card__title">Food name here</div>
-                  <div className="second-card__description">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s. Lorem Ipsum has
-                    been the industry's standard dummy.
-                  </div>
-                  <a href="#" className="second-card__button-continue">
-                    Continue Reading
-                  </a>
-                </div>
-              </div>
-              <div className="homepage-second__devider-line"></div>
-              <div className="homepage-second__card">
-                <div className="second-card__img second-card__img-3">
-                  <div className="card__img--overlay"></div>
-                </div>
-                <div className="second-card__text ">
-                  <div className="second-card__title">Food name here</div>
-                  <div className="second-card__description">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s. Lorem Ipsum has
-                    been the industry's standard dummy text ever since the
-                    1500s.
-                  </div>
-                  <a href="#" className="second-card__button-continue">
-                    Continue Reading
-                  </a>
-                </div>
-              </div>
-              <div className="homepage-second__devider-line"></div>
-            </div>
-          </section>
-        </div>
+      <div className="homepage__title-area">
+        <h1 className="homepage__title-h1">
+          SIMPLE AND QUICK RECIPES FOR EVERYDAY LIFE
+        </h1>
       </div>
+      <section className="homepage-first__container">
+        <div className="homepage-first__wrapper">
+          <div className="homepage-first__card">
+            <div className="card__img card__img-1">
+              <div className="card__img--overlay"></div>
+              <p className="card-label">PASTA</p>
+            </div>
+          </div>
+          <div className="homepage-first__card">
+            <div className="card__img card__img-2">
+              <div className="card__img--overlay"></div>
+              <p className="card-label">SNACKS</p>
+            </div>
+          </div>
+          <div className="homepage-first__card">
+            <div className="card__img card__img-3">
+              <div className="card__img--overlay"></div>
+              <p className="card-label">OVEN</p>
+            </div>
+          </div>
+          <div className="homepage-first__card">
+            <div className="card__img card__img-4">
+              <div className="card__img--overlay"></div>
+              <p className="card-label">SPICY</p>
+            </div>
+          </div>
+        </div>
+        <div className="search-container">
+          <BsSearch className="search-icon" />
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search our recipes"
+          />
+          <span className="search__or-span">OR</span>
+          <span className="search__view-all-span">View all Recipes</span>
+        </div>
+      </section>
+
+      <section className="homepage-second__container">
+        <div className="homepage-second__wrapper">
+          <h1 className="homepage-second__section-title">
+            Latest Quick Recipes
+          </h1>
+          {/*Second section cards */}
+          <div className="homepage-second__card">
+            <div className="second-card__img second-card__img-1">
+              <div className="card__img--overlay"></div>
+            </div>
+            <div className="second-card__text ">
+              <div className="second-card__title">Food name here</div>
+              <div className="second-card__description">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s.
+              </div>
+              <a href="#" className="second-card__button-continue">
+                Continue Reading
+              </a>
+            </div>
+          </div>
+          <div className="homepage-second__devider-line"></div>
+          <div className="homepage-second__card">
+            <div className="second-card__img second-card__img-2">
+              <div className="card__img--overlay"></div>
+            </div>
+            <div className="second-card__text ">
+              <div className="second-card__title">Food name here</div>
+              <div className="second-card__description">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s. Lorem Ipsum has been the industry's
+                standard dummy.
+              </div>
+              <a href="#" className="second-card__button-continue">
+                Continue Reading
+              </a>
+            </div>
+          </div>
+          <div className="homepage-second__devider-line"></div>
+          <div className="homepage-second__card">
+            <div className="second-card__img second-card__img-3">
+              <div className="card__img--overlay"></div>
+            </div>
+            <div className="second-card__text ">
+              <div className="second-card__title">Food name here</div>
+              <div className="second-card__description">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s. Lorem Ipsum has been the industry's
+                standard dummy text ever since the 1500s.
+              </div>
+              <a href="#" className="second-card__button-continue">
+                Continue Reading
+              </a>
+            </div>
+          </div>
+          <div className="homepage-second__devider-line"></div>
+        </div>
+      </section>
     </>
   );
 };
