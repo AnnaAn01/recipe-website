fd5f19d1cf6dfc8f30abc68d8aedc4a9e6f91fc0
Anna An <arzumanyan.an@gmail.com>
2022-06-26 15:54:53 +0400

    styling about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -7,8 +7,25 @@ import homepageMain1 from "../../images-src/homepage-main-1.jpg";
 const AboutPage = () => {
   return (
     <>
-      <section className="homepage-first__container">
-        <div className="homepage-first__wrapper">Something here</div>
+      <section className="about__container">
+        <div className="about__wrapper">
+          <div className="about__photo-wrapper">
+            <div className="about__img"></div>
+          </div>
+          <div className="about__text-wrapper">
+            <div className="about__title">About me</div>
+            <p className="about__first-p">Hi, my name is Anna.</p>
+            <p className="about__text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
+              ex officia? A quibusdam, illo consequatur nulla quaerat ex
+              aliquid, sequi quia numquam id est. Mollitia quidem iure ipsa modi
+              beatae? Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Fugit, ex officia? A quibusdam, illo consequatur nulla quaerat ex
+              aliquid, sequi quia numquam id est. Mollitia quidem iure ipsa modi
+              beatae?
+            </p>
+          </div>
+        </div>
       </section>
     </>
   );
