b07b1b57fc81d0ea2423314bf1c19877caebe48c
Anna An <arzumanyan.an@gmail.com>
2022-07-01 14:37:57 +0400

    adding search results

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import "./SearchBar.css";
+import MOCK_DATA from "./MOCK_DATA.json";
+
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className="search-container">
       <BsSearch className="search-icon" />
@@ -10,8 +14,18 @@ const SearchBar = () => {
         type="text"
         placeholder="Search our recipes"
       />
+
       <span className="search__or-span">OR</span>
       <span className="search__view-all-span">View all Recipes</span>
+      <div className="search__value">
+        {MOCK_DATA.map((val, key) => {
+          return (
+            <div className="search__value-div" key={key}>
+              <p className="search__value-p">{val.first_name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
