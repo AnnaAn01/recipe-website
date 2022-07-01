3e6fc575c701b6662b397891f61e3cb742567c87
Anna An <arzumanyan.an@gmail.com>
2022-07-01 15:13:56 +0400

    making search results clickable links

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import "./SearchBar.css";
 import MOCK_DATA from "./MOCK_DATA.json";
+import { Link } from "react-router-dom";
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -13,15 +14,32 @@ const SearchBar = () => {
         className="search-input"
         type="text"
         placeholder="Search our recipes"
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
       />
 
       <span className="search__or-span">OR</span>
       <span className="search__view-all-span">View all Recipes</span>
       <div className="search__value">
-        {MOCK_DATA.map((val, key) => {
+        {MOCK_DATA.filter((val) => {
+          if (searchTerm === "") {
+            return "";
+          } else if (
+            val.first_name
+              .toLocaleLowerCase()
+              .includes(searchTerm.toLocaleLowerCase())
+          ) {
+            return <Link to="/about">{val}</Link>;
+          }
+        }).map((val, key) => {
           return (
             <div className="search__value-div" key={key}>
-              <p className="search__value-p">{val.first_name}</p>
+              <p className="search__value-p">
+                <Link to="/about" className="search__value-link-returned">
+                  {val.first_name}
+                </Link>
+              </p>
             </div>
           );
         })}
