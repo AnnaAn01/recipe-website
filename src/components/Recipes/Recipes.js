fe2ec07b58dbaccae8e9a922042541b24184ba26
Anna An <arzumanyan.an@gmail.com>
2022-07-14 10:06:40 +0400

    Adding Recipe section

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Recipes.css";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Recipes = () => {
   return (
     <>
       <div className="recipes__container">
-        <div className="recipes__wrapper"></div>
+        <div className="recipes__wrapper">
+          <div className="recipes-search__wrapper">
+            <SearchBar />
+          </div>
+        </div>
       </div>
     </>
   );
