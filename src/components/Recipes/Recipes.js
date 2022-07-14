e272c36cb637ed6574536060a190e570cf08bc1d
Anna An <arzumanyan.an@gmail.com>
2022-07-14 09:50:10 +0400

    Adding Recipe section

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./Recipes.css";
 
 const Recipes = () => {
   return (
     <>
-      <div className="recipes__container"></div>
+      <div className="recipes__container">
+        <div className="recipes__wrapper"></div>
+      </div>
     </>
   );
 };
