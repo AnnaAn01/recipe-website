8dcda0498287eb4841d7d8320320229d21b365a5
Anna An <arzumanyan.an@gmail.com>
2022-07-23 19:17:43 +0400

    adding the RECIPES.json file

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -52,6 +52,63 @@
 
 // export default SearchBar;
 
+// import React, { useState } from "react";
+// import { BsSearch } from "react-icons/bs";
+// import "./SearchBar.css";
+// import RECIPES from "./RECIPES.json";
+// import { Link } from "react-router-dom";
+
+// const SearchBar = () => {
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   return (
+//     <div className="search-container">
+//       <BsSearch className="search-icon" />
+//       <input
+//         className="search-input"
+//         type="text"
+//         placeholder="Search our recipes"
+//         onChange={(event) => {
+//           setSearchTerm(event.target.value);
+//         }}
+//       />
+
+//       <span className="search__or-span">OR</span>
+//       <Link to="recipes" className="search__view-all-link">
+//         <span className="search__view-all-span">View all Recipes</span>
+//       </Link>
+//       <div className="search__value">
+//         {RECIPES.filter((val) => {
+//           if (searchTerm === "") {
+//             return "";
+//           } else if (
+//             val.name
+//               .toLocaleLowerCase()
+//               .includes(searchTerm.toLocaleLowerCase()) ||
+//             val.ingredients[0].name
+//               .toLocaleLowerCase()
+//               .includes(searchTerm.toLocaleLowerCase())
+//           ) {
+//             return <Link to="/about">{val}</Link>;
+//           }
+//         }).map((val, key) => {
+//           return (
+//             <div className="search__value-div" key={key}>
+//               <p className="search__value-p">
+//                 <Link to="/about" className="search__value-link-returned">
+//                   {val.name}
+//                 </Link>
+//               </p>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SearchBar;
+
 import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import "./SearchBar.css";
@@ -84,12 +141,9 @@ const SearchBar = () => {
           } else if (
             val.name
               .toLocaleLowerCase()
-              .includes(searchTerm.toLocaleLowerCase())
-          ) {
-            return <Link to="/about">{val}</Link>;
-          } else if (
-            val.ingredients.name
-              .toLowerCase()
+              .includes(searchTerm.toLocaleLowerCase()) ||
+            val.ingredients[0].name
+              .toLocaleLowerCase()
               .includes(searchTerm.toLocaleLowerCase())
           ) {
             return <Link to="/about">{val}</Link>;
