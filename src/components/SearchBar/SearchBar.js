44a0697c0b46b5677e28927393d758cd2bc664c0
Anna An <arzumanyan.an@gmail.com>
2022-07-23 18:41:01 +0400

    adding recipes

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,61 @@
+// import React, { useState } from "react";
+// import { BsSearch } from "react-icons/bs";
+// import "./SearchBar.css";
+// import MOCK_DATA from "./MOCK_DATA.json";
+// import { Link } from "react-router-dom";
+
+// const SearchBar = () => {
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   return (
+//     <div className="search-container">
+//       <BsSearch className="search-icon" />
+//       <input
+//         className="search-input"
+//         type="text"
+//         placeholder="Search our recipes"
+//         onChange={(event) => {
+//           setSearchTerm(event.target.value);
+//         }}
+//       />
+
+//       <span className="search__or-span">OR</span>
+//       <Link to="recipes" className="search__view-all-link">
+//         <span className="search__view-all-span">View all Recipes</span>
+//       </Link>
+//       <div className="search__value">
+//         {MOCK_DATA.filter((val) => {
+//           if (searchTerm === "") {
+//             return "";
+//           } else if (
+//             val.first_name
+//               .toLocaleLowerCase()
+//               .includes(searchTerm.toLocaleLowerCase())
+//           ) {
+//             return <Link to="/about">{val}</Link>;
+//           }
+//         }).map((val, key) => {
+//           return (
+//             <div className="search__value-div" key={key}>
+//               <p className="search__value-p">
+//                 <Link to="/about" className="search__value-link-returned">
+//                   {val.first_name}
+//                 </Link>
+//               </p>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SearchBar;
+
 import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import "./SearchBar.css";
-import MOCK_DATA from "./MOCK_DATA.json";
+import RECIPES from "./RECIPES.json";
 import { Link } from "react-router-dom";
 
 const SearchBar = () => {
@@ -24,12 +78,18 @@ const SearchBar = () => {
         <span className="search__view-all-span">View all Recipes</span>
       </Link>
       <div className="search__value">
-        {MOCK_DATA.filter((val) => {
+        {RECIPES.filter((val) => {
           if (searchTerm === "") {
             return "";
           } else if (
-            val.first_name
+            val.name
               .toLocaleLowerCase()
+              .includes(searchTerm.toLocaleLowerCase())
+          ) {
+            return <Link to="/about">{val}</Link>;
+          } else if (
+            val.ingredients.name
+              .toLowerCase()
               .includes(searchTerm.toLocaleLowerCase())
           ) {
             return <Link to="/about">{val}</Link>;
@@ -39,7 +99,7 @@ const SearchBar = () => {
             <div className="search__value-div" key={key}>
               <p className="search__value-p">
                 <Link to="/about" className="search__value-link-returned">
-                  {val.first_name}
+                  {val.name}
                 </Link>
               </p>
             </div>
