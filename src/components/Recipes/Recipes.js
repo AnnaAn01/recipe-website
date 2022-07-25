8ac00be40b12effbea598a5b4ee8db602c95024c
Anna An <arzumanyan.an@gmail.com>
2022-07-25 18:22:28 +0400

    adding the recipe list grid

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -15,7 +15,22 @@ const Recipes = () => {
             <div className="recipes__all-wrapper">
               <div className="recipes__all-title">ALL RECIPES</div>
               <div className="recipes__all-list-container">
-                <div className="recipes__all-list-wrapper"></div>
+                <div className="recipes__all-list-wrapper">
+                  <div class="recipes__list-parent">
+                    <div class="recipes__list recipes__list-1"> </div>
+                    <div class="recipes__list recipes__list-2"> </div>
+                    <div class="recipes__list recipes__list-3"> </div>
+                    <div class="recipes__list recipes__list-4"> </div>
+                    <div class="recipes__list recipes__list-5"> </div>
+                    <div class="recipes__list recipes__list-6"> </div>
+                    <div class="recipes__list recipes__list-7"> </div>
+                    <div class="recipes__list recipes__list-8"> </div>
+                    <div class="recipes__list recipes__list-9"> </div>
+                    <div class="recipes__list recipes__list-10"> </div>
+                    <div class="recipes__list recipes__list-11"> </div>
+                    <div class="recipes__list recipes__list-12"> </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
