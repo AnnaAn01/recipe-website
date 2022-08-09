bc254d74fd7173d762eb45188e2892b69a0f5912
Anna An <arzumanyan.an@gmail.com>
2022-08-09 08:20:45 +0400

    styling About page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,10 +1,7 @@
 import React from "react";
 import "./AboutPage.css";
 import SearchBar from "../SearchBar/SearchBar";
-
-// import aboutImg from "./about-img-1.jpg";
 import aboutImg from "../../images-src/about-img-1.jpg";
-// import aboutImg from "../../images-src/homepage-main-4.jpg";
 
 const AboutPage = () => {
   return (
@@ -12,9 +9,7 @@ const AboutPage = () => {
       <section className="about__container">
         <div className="about__wrapper">
           <div className="about__photo-wrapper">
-            {/*<img src="homepageMain1" alt="about me" className="about__img" />*/}
             <img src={aboutImg} alt="about me" className="about__img" />
-            {/*<div className="about__img"></div>*/}
           </div>
           <div className="about__text-wrapper">
             <div className="about__text-title">About me</div>
