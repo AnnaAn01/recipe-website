a4b3bb9a83089174fb682e3d567445529f3563ae
Anna An <arzumanyan.an@gmail.com>
2022-08-14 10:06:55 +0400

    adding pagination

--- src/components/Recipes/Pagination.js
+++ src/components/Recipes/Pagination.js
@@ -1,6 +1,40 @@
+// import { Link } from "react-router-dom";
+// import "./Recipes.css";
+// import React from "react";
+
+// const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
+//   const pageNumbers = [];
+
+//   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+//     pageNumbers.push(i);
+//   }
+
+//   return (
+//     <div className="pagination-container">
+//       <ul className="pagination">
+//         {pageNumbers.map((number) => (
+//           <li key={number} className="page-item">
+//             <Link
+//               onClick={() => paginate(number)}
+//               to="/recipe"
+//               className="page-link"
+//             >
+//               {number}
+//             </Link>
+//             {console.log("this", number)}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Pagination;
+
 import { Link } from "react-router-dom";
 import "./Recipes.css";
 import React from "react";
+import ReactPaginate from "react-paginate";
 
 const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
   const pageNumbers = [];
@@ -10,7 +44,7 @@ const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
   }
 
   return (
-    <div className="pagnation-container">
+    <div className="pagination-container">
       <ul className="pagination">
         {pageNumbers.map((number) => (
           <li key={number} className="page-item">
@@ -29,3 +63,63 @@ const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
 };
 
 export default Pagination;
+
+// import { Link } from "react-router-dom";
+// import "./Recipes.css";
+// import React from "react";
+// import ReactPaginate from "react-paginate";
+
+// const Pagination = ({
+//   posts,
+//   postsPerPage,
+//   totalPosts,
+//   paginate,
+//   setCurrentPage,
+//   currentPage,
+// }) => {
+//   // const pageNumbers = [];
+//   const pageNumbers = Math.ceil(totalPosts / postsPerPage);
+//   // console.log(pageNumbers);
+//   // console.log(totalPosts);
+//   // console.log(postsPerPage);
+
+//   // for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+//   //   pageNumbers.push(i);
+//   // }
+
+//   const changePage = ({ selected }) => {
+//     paginate(selected);
+//   };
+
+//   return (
+//     <ReactPaginate
+//       previousLabel={"Previous"}
+//       nextLabel={"Next"}
+//       pageCount={pageNumbers}
+//       onPageChange={changePage}
+//       containerClassName={"paginationBttns"}
+//       previousLinkClassName={"previousBttn"}
+//       nextLinkClassName={"nextBttn"}
+//       disabledClassName={"paginationDisabled"}
+//       activeClassName={"paginationActive"}
+//     />
+//     // <div className="pagination-container">
+//     //   <ul className="pagination">
+//     //     {pageNumbers.map((number) => (
+//     //       <li key={number} className="page-item">
+//     //         <Link
+//     //           onClick={() => paginate(number)}
+//     //           to="/recipe"
+//     //           className="page-link"
+//     //         >
+//     //           {number}
+//     //         </Link>
+//     //         {console.log("this", number)}
+//     //       </li>
+//     //     ))}
+//     //   </ul>
+//     // </div>
+//   );
+// };
+
+// export default Pagination;
