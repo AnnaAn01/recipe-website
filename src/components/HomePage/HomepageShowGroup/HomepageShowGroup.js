7a699c1d36359be886748365a11be62e5b6d0fdb
Anna An <arzumanyan.an@gmail.com>
2022-09-09 09:32:47 +0400

    adding home page show group details

--- src/components/HomePage/HomepageShowGroup/HomepageShowGroup.js
+++ src/components/HomePage/HomepageShowGroup/HomepageShowGroup.js
@@ -5,19 +5,14 @@ import { Link } from "react-router-dom";
 // import { useParams } from "react-router-dom";
 
 const HomepageShowGroup = () => {
-  //   const params = useParams();
-  // getting the clicked post id from the router
-  //   let id = Number(params.postid.substring(1));
-  // console.log(typeof id);
-  // console.log(id);
-
   const foodGroups = RECIPES.map((el, index) => {
     if (el.group === "pasta") {
       return (
         <Link
           // to show the item numbers on pages 2 and 3
           to={`/recipe-pages/:${
-            el.id <= 12 ? index : 12 * (el.page - 1) + index
+            index
+            // el.id <= 12 ? index : 12 * (el.page - 1) + index
             // Or we can use the page I manually added to the json file
             // el.page === 1 ? index : 12 * (el.page - 1) + index
           }`}
