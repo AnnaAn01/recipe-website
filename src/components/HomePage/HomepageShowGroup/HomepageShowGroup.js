570cee8a0c97aeb14b958c4a2a4e520edc34e564
Anna An <arzumanyan.an@gmail.com>
2022-09-09 11:35:34 +0400

    making home page first section dynamic

--- src/components/HomePage/HomepageShowGroup/HomepageShowGroup.js
+++ src/components/HomePage/HomepageShowGroup/HomepageShowGroup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import RECIPES from "../../SearchBar/RECIPES.json";
+import HomePageSectionOne from "../HomePageSectionOne.json";
 import "./HomepageShowGroup.css";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
@@ -7,12 +8,25 @@ import { useParams } from "react-router-dom";
 const HomepageShowGroup = () => {
   const params = useParams();
   let id = params.postid.substring(1);
+  // let id = Number(params.postid.substring(1));
+  let hpSectionOneId =
+    id === "pasta"
+      ? 0
+      : id === "snacks"
+      ? 1
+      : id === "oven"
+      ? 2
+      : id === "spicy"
+      ? 3
+      : "";
+
   console.log("id inside HSG", id);
+  console.log(HomePageSectionOne[0].imagePath);
 
   const foodGroups = RECIPES.map((el, index) => {
-    if (el.group === "pasta") {
+    if (id === el.group) {
       return (
-        <Link to={`/recipe-pages/:${index}`} className="recipes__list-link">
+        <Link to={`/recipe-pages/:${id}`} className="recipes__list-link">
           <div
             name={el.name}
             key={el.index}
@@ -33,12 +47,17 @@ const HomepageShowGroup = () => {
     <div className="homapage-show-group">
       <div className="show-group__container">
         <div className="show-group__intro-wrapper">
-          <div className="show-group__intro-img-wrapper">
-            <img src="" alt="food" className="show-group__intro-img" />
+          <div className="show-group__intro-img-wrapper"></div>
+          <img
+            src={HomePageSectionOne[hpSectionOneId].imagePath}
+            alt="food"
+            className="show-group__intro-img"
+          />
+          <div className="show-group__intro-title">
+            {HomePageSectionOne[hpSectionOneId].label} Recipes
           </div>
-          <div className="show-group__intro-title">Pasta Recipes</div>
+          {foodGroups}
         </div>
-        {foodGroups}
       </div>
     </div>
   );
