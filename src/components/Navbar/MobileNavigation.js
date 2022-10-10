8e53fc2e323fb44f422642e854563f3a9241b524
Anna An <arzumanyan.an@gmail.com>
2022-10-10 08:52:28 +0400

    adding hamburger menu

--- src/components/Navbar/MobileNavigation.js
+++ src/components/Navbar/MobileNavigation.js
@@ -4,6 +4,7 @@ import { FaHamburger } from "react-icons/fa";
 import { AiFillCloseSquare } from "react-icons/ai";
 import { CgMenuHotdog } from "react-icons/cg";
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const MobileNavigation = () => {
   const openHamburgerIcon = (
@@ -19,12 +20,23 @@ const MobileNavigation = () => {
       onClick={() => setOpenMobileMenu(!openMobileMenu)}
     />
   );
+
+  const closeMobileMenuAfterClick = () => setOpenMobileMenu(false);
+
   const [openMobileMenu, setOpenMobileMenu] = useState(false);
   return (
     <nav className="navbar__mobile-hamburger">
+      <NavLink to="/" className="navbar__title">
+        <div className="navbar__title">Anna's Kitchen</div>
+      </NavLink>
       {openMobileMenu ? closeHamburgerIcon : openHamburgerIcon}
       {/*If the value of  openMobileMenu is true then show <NavLinks/>*/}
-      {openMobileMenu && <NavLinks />}
+      {openMobileMenu && (
+        <NavLinks
+          isMobile={true}
+          closeMobileMenuAfterClick={closeMobileMenuAfterClick}
+        />
+      )}
     </nav>
   );
 };
--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -1,19 +1,40 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-const NavLinks = () => {
+import { motion } from "framer-motion";
+
+const NavLinks = (props) => {
+  const animateFrom = { opacity: 0, y: -100 };
+  const animateTo = { opacity: 1, y: 0 };
+
   return (
-    <div className="navbar__item-link-wrapper">
-      <NavLink to="/" className="navbar__item-link">
+    <motion.div
+      className="navbar__item-link-wrapper"
+      initial={animateFrom}
+      animate={animateTo}
+    >
+      <NavLink
+        to="/"
+        className="navbar__item-link"
+        onClick={() => props.isMobile && props.closeMobileMenuAfterClick()}
+      >
         <div className="navbar__item">HOME</div>
       </NavLink>
 
-      <NavLink to="/recipe" className="navbar__item-link">
+      <NavLink
+        to="/recipe"
+        className="navbar__item-link"
+        onClick={() => props.isMobile && props.closeMobileMenuAfterClick()}
+      >
         <div className="navbar__item">RECIPES</div>
       </NavLink>
-      <NavLink to="/about" className="navbar__item-link">
+      <NavLink
+        to="/about"
+        className="navbar__item-link"
+        onClick={() => props.isMobile && props.closeMobileMenuAfterClick()}
+      >
         <div className="navbar__item">ABOUT</div>
       </NavLink>
-    </div>
+    </motion.div>
   );
 };
 
