86ea011fdc58c583a3f2abaafe85d20e95bbaa98
Anna An <arzumanyan.an@gmail.com>
2022-11-28 09:57:48 +0400

    workng on the custom slider

--- src/components/HomePage/HomeCarousel/Carousel.js
+++ src/components/HomePage/HomeCarousel/Carousel.js
@@ -105,26 +105,111 @@
 //   );
 // }
 
+// import React, { useState } from "react";
+
+// function Carousel() {
+//   const initalState = [
+//     { idx: 1, pos: 1, text: "1", active: true },
+//     { idx: 2, pos: 2, text: "2", active: true },
+//     { idx: 3, pos: 3, text: "3", active: true },
+//     { idx: 4, pos: 4, text: "4", active: true },
+//     { idx: 5, pos: 5, text: "5", active: false },
+//   ];
+
+//   const slides = [
+//     { url: "recipe-img/homepage-pasta.jpg", id: 0 },
+//     { url: "recipe-img/homepage-main-6.jpg", id: 1 },
+//     { url: "recipe-img/homepage-main-7.jpg", id: 3 },
+//     { url: "recipe-img/homepage-main-8.jpg", id: 4 },
+//     { url: "recipe-img/curried-chicken-salad.jpg", id: 5 },
+//     { url: "recipe-img/curried-chicken-salad.jpg", id: 6 },
+//   ];
+
+//   const [cards, setCards] = useState(initalState);
+
+//   const handleLeftClick = (isLeft) => {
+//     const prevState = [...cards];
+//     // find next inactive card index - top
+//     const nextCardIdx = prevState
+//       .filter((ft) => ft.active === true)
+//       .sort((a, b) => (a.pos > b.pos ? 1 : b.pos > a.pos ? -1 : 0))[0].idx;
+//     // reset
+//     prevState.find((f) => f.active === false).active = true;
+//     // update
+//     prevState.find((f) => f.idx === nextCardIdx).active = false;
+//     // maximize pos
+//     prevState.find((f) => f.idx === nextCardIdx).pos =
+//       Math.max.apply(
+//         null,
+//         prevState.map(function (o) {
+//           return o.pos;
+//         })
+//       ) + 1;
+
+//     // update state
+//     setCards(prevState);
+//   };
+
+//   const handleRightClick = () => {
+//     const prevState = [...cards];
+//     // find next inactive card index - bottom
+//     const nextCardIdx = prevState
+//       .filter((ft) => ft.active === true)
+//       .sort((a, b) => (a.pos > b.pos ? 1 : b.pos > a.pos ? -1 : 0))
+//       .pop(1).idx;
+//     // minimize pos
+//     prevState.find((f) => f.active === false).pos =
+//       Math.min.apply(
+//         null,
+//         prevState.map(function (o) {
+//           return o.pos;
+//         })
+//       ) - 1;
+//     // reset
+//     prevState.find((f) => f.active === false).active = true;
+//     // update
+//     prevState.find((f) => f.idx === nextCardIdx).active = false;
+
+//     // update state
+//     setCards(prevState);
+//   };
+
+//   return (
+//     <>
+//       <div
+//         className="text-xl md:text-5xl cursor-pointer"
+//         onClick={() => handleLeftClick()}
+//       >
+//         {"<"}
+//       </div>
+//       {cards
+//         .filter((f) => f.active === true)
+//         .sort((a, b) => (a.pos > b.pos ? 1 : b.pos > a.pos ? -1 : 0))
+//         .map((card, index) => (
+//           <img
+//             src={slides[index].url}
+//             key={index}
+//             prop={card.text}
+//             className="homepage-first__card"
+//           ></img>
+//         ))}
+//       <div
+//         className="text-xl md:text-5xl cursor-pointer"
+//         onClick={() => handleRightClick()}
+//       >
+//         {">"}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Carousel;
+
 import React, { useState } from "react";
+import Card from "./Card";
+import { initalState } from "./data";
 
 function Carousel() {
-  const initalState = [
-    { idx: 1, pos: 1, text: "1", active: true },
-    { idx: 2, pos: 2, text: "2", active: true },
-    { idx: 3, pos: 3, text: "3", active: true },
-    { idx: 4, pos: 4, text: "4", active: true },
-    { idx: 5, pos: 5, text: "5", active: false },
-  ];
-
-  const slides = [
-    { url: "recipe-img/homepage-pasta.jpg", id: 0 },
-    { url: "recipe-img/homepage-main-6.jpg", id: 1 },
-    { url: "recipe-img/homepage-main-7.jpg", id: 3 },
-    { url: "recipe-img/homepage-main-8.jpg", id: 4 },
-    { url: "recipe-img/curried-chicken-salad.jpg", id: 5 },
-    { url: "recipe-img/curried-chicken-salad.jpg", id: 6 },
-  ];
-
   const [cards, setCards] = useState(initalState);
 
   const handleLeftClick = (isLeft) => {
@@ -177,7 +262,7 @@ function Carousel() {
   return (
     <>
       <div
-        className="text-xl md:text-5xl cursor-pointer"
+        className="carousel-cont text-xl md:text-5xl cursor-pointer"
         onClick={() => handleLeftClick()}
       >
         {"<"}
@@ -186,12 +271,7 @@ function Carousel() {
         .filter((f) => f.active === true)
         .sort((a, b) => (a.pos > b.pos ? 1 : b.pos > a.pos ? -1 : 0))
         .map((card, index) => (
-          <img
-            src={slides[index].url}
-            key={index}
-            prop={card.text}
-            className="homepage-first__card"
-          ></img>
+          <Card key={index} prop={card.img} />
         ))}
       <div
         className="text-xl md:text-5xl cursor-pointer"
---
+++ src/components/HomePage/HomeCarousel/data.js
@@ -0,0 +1,31 @@
+// inital data - dynamic data strutre works
+export const initalState = [
+  {
+    idx: 1,
+    pos: 1,
+    text: "1",
+    img: "recipe-img/homepage-pasta.jpg",
+    active: true,
+  },
+  {
+    idx: 2,
+    pos: 2,
+    text: "2",
+    img: "recipe-img/homepage-main-6.jpg",
+    active: true,
+  },
+  {
+    idx: 3,
+    pos: 3,
+    text: "3",
+    img: "recipe-img/homepage-main-7.jpg",
+    active: true,
+  },
+  {
+    idx: 4,
+    pos: 4,
+    text: "4",
+    img: "recipe-img/homepage-main-8.jpg",
+    active: false,
+  },
+];
